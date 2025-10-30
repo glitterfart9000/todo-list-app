e221a8b39dfd9ac8d40576aefbb023b9dba21a86
glitterfart9000 <jacobcampcs@gmail.com>
2025-10-30 17:51:51 +0000

    implemented add task

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log("Script started");
+
+function addTask(event) {
+    // get user input from text box
+    let textbox = document.getElementById("task-input");
+    let task = textbox.value;
+    console.log(task);
+
+    // create a list item
+    // get todo-list container div
+    let todoList = document.getElementById("todo-list");
+
+    // create list-item div
+    let listDiv = document.createElement("div");
+    listDiv.id = "task" + todoList.childElementCount;
+    listDiv.classList.add("list-item");
+
+    // create checkbox
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "checkbox" + todoList.childElementCount;
+
+    // create label
+    let label = document.createElement("label");
+    label.id = "label" + todoList.childElementCount;
+    label.innerText = task;
+
+    // append checkbox to list-item div
+    listDiv.appendChild(checkbox);
+
+    // append label to div
+    listDiv.appendChild(label);
+
+    // append list-item div to the list
+    todoList.appendChild(listDiv);
+}
