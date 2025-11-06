582f5976076a3bc5c0a9167e40165ef48109d126
glitterfart9000 <jacobcampcs@gmail.com>
2025-11-06 17:26:19 +0000

    animated remove task

--- script.js
+++ script.js
@@ -45,6 +45,11 @@ function removeTask(event) {
     // get task div by id
     let taskDiv = document.getElementById("task" + idNum);
 
+    // apply animation
+    taskDiv.classList.add("remove-task");
+
     // remove the task div from layout
-    taskDiv.remove();
+    setTimeout(function() {
+        taskDiv.remove();
+    }, 1000);
 }
